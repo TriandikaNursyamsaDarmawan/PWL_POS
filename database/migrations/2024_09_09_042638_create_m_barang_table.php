cda6517571ee5cd188973df732e6ec9dea33d931
TriandikaNursyamsaDarmawan <andikadarmawan43@gmail.com>
2024-09-09 11:38:24 +0700

    Praktikum2.2

---
+++ database/migrations/2024_09_09_042638_create_m_barang_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+
+     public function up(): void
+     {
+         Schema::create('m_kategori', function (Blueprint $table) {
+             $table->id('lvl_id');
+             $table->string('lvl_kode',10)->unique();
+             $table->string('lvl_nama',100);
+             $table->timestamps();
+         });
+     }
+    public function up(): void
+    {
+        Schema::create('m_barang', function (Blueprint $table) {
+            $table->id('barang_id');
+            $table->unsignedBigInteger('kategori_id',20);
+            $table->string('barang_nama',100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('m_barang');
+    }
+};
