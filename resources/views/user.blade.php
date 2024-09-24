6376eb3fbab978623d5513acb0d5a8bc8c3192d1
TriandikaNursyamsaDarmawan <andikadarmawan43@gmail.com>
2024-09-24 23:48:39 +0700

    Laporan2.7

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -5,7 +5,7 @@
     </head>
     <body>
         
-        <h1> Data Level Pengguna</h1>
+        <h1> Data User</h1>
         
     <a href="{{url('/user/tambah')}}">Tambah Data User</a>
         <table border="1" cellpadding="2" cellspasing="0">
@@ -14,13 +14,18 @@
                 <th>Username</th>
                 <th>Nama</th>
                 <th>ID Level Pengguna</th>
+                <th>Kode Level</th>
+                <th>Nama Level</th>
+                <th>Aksi</th>
             </tr>
             @foreach ($data as $d)
             <tr>
                 <td>{{$d->user_id}}</td>
                 <td>{{$d->username}}</td>
                 <td>{{$d->nama}}</td>
-                <td>{{$d->level}}</td>
+                <td>{{$d->level_id}}</td>
+                <td>{{$d->level->level_kode}}</td>
+                <td>{{$d->level->level_nama}}</td>
                 <td><a href="/user/ubah/{{ $d->user_id}}">Ubah</a> | <a href="/user/hapus/{{$d->user_id}}">Hapus</a></td>
             </tr>
             @endforeach
