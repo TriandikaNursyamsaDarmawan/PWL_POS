39a1c7bb1821d60d34f4c16e6f0c74b4d3038933
TriandikaNursyamsaDarmawan <andikadarmawan43@gmail.com>
2024-10-07 05:51:56 +0700

    JS6-Praktikum1

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,10 +1,12 @@
 @extends('layouts.template')
+
 @section('content')
 <div class="card card-outline card-primary">
-<div class="card-header">
-<h3 class="card-title">{{ $page->title }}</h3>
+    <div class="card-header">
+        <h3 class="card-title">{{ $page->title }}</h3>
 <div class="card-tools">
 <a class="btn btn-sm btn-primary mt-1" href="{{ url('user/create') }}">Tambah</a>
+<button onclick="modalAction('{{url('user/create_ajax')}}')" class="btn btn-sm btn-success mt-1">Tambah Ajax</button>
 </div>
 </div>
 
@@ -38,11 +40,18 @@
 </table>
 </div>
 </div>
+<div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" data-
+backdrop="static" data-keyboard="false" data-width="75%" aria-hidden="true"></div>
 @endsection
 @push('css')
 @endpush
 
 @push('js')
+function modalAction(url =''){
+    $('#myModal').load(url,function(){
+    $('#myModal').modal('show');
+    });
+    }
 <script>
 $(document).ready(function() {
 var dataUser = $('#table_user').DataTable({
